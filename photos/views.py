25465b807c20814e20d6e1371653c300a6f5a6e2
Teresiah Githua <terrywambo2018@gmail.com>
2022-05-30 01:21:24 +0300

    ft:this will add photo categories functionality

--- photos/views.py
+++ photos/views.py
@@ -1,9 +1,13 @@
+from multiprocessing import context
+from unicodedata import category
 from django.shortcuts import render
-
+from .models import Category, Photo
 # Create your views here.
 
 def gallery(request):
-    return render(request, 'photos/gallery.html')
+    categories = Category.objects.all()
+    context = {'categories': categories}
+    return render(request, 'photos/gallery.html', context)
 
 def viewphoto(request):
     return render(request, 'photos/photo.html')
