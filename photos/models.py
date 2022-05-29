335d830d2d7121ecc8932142a523364d30a967b2
Teresiah Githua <terrywambo2018@gmail.com>
2022-05-29 19:04:04 +0300

    ft;this will create models

--- photos/models.py
+++ photos/models.py
@@ -1,3 +1,23 @@
+from email.mime import image
+from pyexpat import model
+from typing_extensions import Self
+from unicodedata import category, name
 from django.db import models
 
 # Create your models here.
+class Category(models.Model);
+    name = models.CharField(max_length=100, null=False, blank=False)
+
+
+    def _str_ (self):
+        return self.name
+
+class Photo(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL)
+    image = models.ImageField(null=False, blank=False)
+    description = models.CharField(max_length=500, null=False, blank=False)
+
+    def __str__(self):
+        return self.name        
+
+
