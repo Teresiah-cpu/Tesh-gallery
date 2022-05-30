174cee4ee4e17f0abe0cf7ba57b778d3e17f4a8c
Teresiah Githua <terrywambo2018@gmail.com>
2022-05-30 03:43:56 +0300

    ft this will edit models

--- photos/models.py
+++ photos/models.py
@@ -4,6 +4,7 @@ from pyexpat import model
 from unicodedata import category, name
 from django.db import models
 
+
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=100, null=False, blank=True)
@@ -13,7 +14,8 @@ class Category(models.Model):
         return self.name
 
 class Photo(models.Model):
-    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, null=True, blank=True)
     image = models.ImageField(null=False, blank=False)
     description = models.TextField()
 
