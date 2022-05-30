9182a8f9ad1f0827603ccecdb348c3910797a600
Teresiah Githua <terrywambo2018@gmail.com>
2022-05-30 04:52:14 +0300

    ft:add a form to add image

--- photos/urls.py
+++ photos/urls.py
@@ -1,7 +1,7 @@
 from django.urls import re_path as url
 from . import views
 
-urlpatterns=[
+urlpatterns= [
     url(r'^$',views.gallery,name='gallery'),
     url('photo/<str:pk>/',views.viewphoto, name='photo'),
     url('add/', views.addphoto, name='add'),
--- photos/views.py
+++ photos/views.py
@@ -8,11 +8,15 @@ def gallery(request):
     categories = Category.objects.all()
     photos =Photo.objects.all()
     context = {'categories': categories, 'photos': photos}
-    return render(request,'photos/gallery.html', context)
+    return render(request, 'photos/gallery.html', context)
 
 def viewphoto(request):
     photo = Photo.objects.all()
     return render(request, 'photos/photo.html', {'photo': photo})
+
+# def viewphoto(request, pk):
+#     photo = Photo.objects.get(id=pk)
+#     return render(request, 'photos/photo.html', {'photo': photo})
 
 def addphoto(request):
     return render(request, 'photos/add.html')            
